d173d0ae567b5b5b01115bfdb84acefc262ddbc4
Michael Irwin <mikesir87@gmail.com>
2025-01-10 16:51:48 -0500

    Revert "preparing demo"
    This reverts commit 8a402be1dff685626c05ecae9a8efc74ca66ea15.

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -49,8 +49,8 @@ async function createProduct(product) {
     action: "product_created",
     id: newProductId,
     name: product.name,
-    price: product.price,
     upc: product.upc,
+    price: product.price,
   });
 
   return {
