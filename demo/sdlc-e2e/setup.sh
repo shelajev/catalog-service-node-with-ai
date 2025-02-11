3e7509af4d1fb563dc5ec7ab69e1621325861590
shelajev <shelajev@gmail.com>
2025-02-11 21:20:16 +0200

    We need the demo to work!

--- demo/sdlc-e2e/setup.sh
+++ demo/sdlc-e2e/setup.sh
@@ -2,19 +2,14 @@
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-BRANCH_NAME=demo-$(date +%Y%d%m)-$(whoami)
 
 cd $REPO_ROOT
 
-echo "==> Setting up branch a demo branch named ${BRANCH_NAME}"
+echo "==> Setting up main from remote, cleaning up local changes or unpushed commits"
 git clean -f
 git branch -D temp 2>/dev/null || true
-git branch -D $BRANCH_NAME 2>/dev/null || true
 git checkout -b temp
 git branch -D main
-git checkout main
-git branch -D temp
-git pull
 git checkout -b main
 
 echo "==> Applying patch and creating a commit"
